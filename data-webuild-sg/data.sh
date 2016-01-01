94b76a5e2dffb25cb263632d4d6c17be65df5bee
Sayanee <sayanee@gmail.com>
2016-01-01 19:43:16 +0800

    add talk for data.webuild.sg

---
+++ data-webuild-sg/data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# slides for talk on data.webuild.sg
+
+# whoami
+open http://sayan.ee
+open http://data.webuild.sg
+
+# project background
+open https://github.com/webuildsg/webuild/commits/master?page=34
+open https://webuild.sg
+
+# project motivation
+open https://github.com/webuildsg/webuild/issues/59
+open https://github.com/webuildsg/data
+open https://github.com/webuildsg/data/commits/gh-pages?page=26
+
+# api enpoints
+open https://webuild.sg/api/v1/repos
+open https://webuild.sg/api/v1/events
+open http://data.webuild.sg
+
+# data gathered and plotted
+open http://data.webuild.sg/dataset/repos-per-week/
+open http://data.webuild.sg/dataset/repos-update-activities-per-programming/
+open http://data.webuild.sg/dataset/events-per-group/
+open http://data.webuild.sg/dataset/repos-per-programming-language/
+open http://data.webuild.sg/dataset/events-per-location/
+
+# hwo to contribute
+open https://github.com/webuildsg/data/issues
+open http://data.webuild.sg/
---
+++ data-webuild-sg/readme.md
@@ -0,0 +1,7 @@
+# data.webuild.sg
+
+## quick start
+
+1. make the shell script executable `chmod u+x data.sh`
+- execute the shell script `./data.sh`
+- slides are basically a bunch of websites
