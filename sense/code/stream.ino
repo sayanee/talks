8708330391c01b2e0c2fff6d62e4b6c30676524e
Sayanee <sayanee@gmail.com>
2016-06-12 19:34:28 +0800

    add lipo batt soc and on board LED blinking

--- sense/code/stream.ino
+++ sense/code/stream.ino
@@ -1,13 +1,16 @@
-#include "HttpClient/HttpClient.h"
-#include "lib.h"
+#include "SparkFunMAX17043/SparkFunMAX17043.h" // for lipo charger
+#include "HttpClient/HttpClient.h" // for http
+#include "lib.h" // for proximity sensor
 
-int analogvalue = 0;
-char analogStr[100];
+int led = D7;
+
 double cm = 0.0;
 int trigPin = D4;
 int echoPin = D5;
 char distance[10];
-unsigned long old_time = millis();
+
+double soc = 0;
+
 HC_SR04 rangefinder = HC_SR04(trigPin, echoPin);
 
 HttpClient http;
@@ -22,7 +25,11 @@ http_response_t response;
 
 void setup()
 {
+    lipo.begin();
+    lipo.quickStart();
+    lipo.setThreshold(10);
 
+    pinMode(led, OUTPUT);
 }
 
 void loop()
@@ -31,14 +38,21 @@ void loop()
     sprintf(distance, "%.f", cm);
     Spark.publish("distance", distance);
     sendDistance(distance);
+    digitalWrite(led, HIGH);
     delay(1000);
+    digitalWrite(led, LOW);
+    delay(2000);
 }
 
 void sendDistance(char* distance) {
     char path[200];
     sprintf(path, "/update?key=7JGL56FZ2CBZCHZE&field1=%s", distance);
-    Serial.println(distance);
+
+    soc = lipo.getSOC();
+
     Serial.println(path);
+    Serial.println(soc);
+
     request.hostname = "10.0.1.22";
     request.port = 3000;
     request.path = path;
